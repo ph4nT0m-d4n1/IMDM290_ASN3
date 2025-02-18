79f3788b2df011ddd115e85926c2330ecb5b4e62
maialee <maialee8@terpmail.umd.edu>
2025-02-18 10:45:19 -0500

    more cubes

--- Assets/Scripts/CubeGrow.cs
+++ Assets/Scripts/CubeGrow.cs
@@ -3,15 +3,22 @@ using UnityEngine;
 public class CubeGrow : MonoBehaviour
 {
     float time;
+    float x;
+    float y;
+    float z;
+    
+   
     void Start()
     {
-        
+        x = gameObject.transform.localScale.x;
+        y = gameObject.transform.localScale.y;
+        z = gameObject.transform.localScale.z;
     }
 
     // Update is called once per frame
     void Update()
     {
         time += Time.deltaTime;
-        gameObject.transform.localScale = new Vector3(1f, 1f * time, 1f);
+        gameObject.transform.localScale = new Vector3(x,  y* time, z);
     }
 }
